359ac937c7d3a87f2cf420c902269fa012f9579c
Michael Garrido <michael.a.garrido@gmail.com>
2022-12-26 02:09:54 -0800

    WIP Agent class

--- agent.gd
+++ agent.gd
@@ -25,6 +25,8 @@ class_name Agent
 # ex. body changes position from before action executed to after action
 
 var metabot:Metabot = null
+var metabots = []
+
 var motor = null
 
 var inputs = []
@@ -34,10 +36,32 @@ var actuators = {}
 var motors = {}
 var actions = []
 
+var timer: Timer = null
+var tick_interval = 0.3
+
+func initiate_timer():
+	timer = Timer.new()
+	timer.set_one_shot(false)
+	timer.connect("timeout", self.tick)
+	add_child(timer)
+	# autostart
+	timer.start(tick_interval)
+	
+## Time ticks for the Agent when it is conscious.
+## When it is unconscious/sleeping, time does not tick.
+func tick():
+	print("TICK.")
+#	for mbot in metabots:
+#		mbot.tick()
+	process_input()
+	create_command()
+	
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 #	pass # Replace with function body.
 	print("Agent ready")
+	initiate_timer()
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -45,14 +69,41 @@ func _process(delta):
 	
 func attach_metabot(mbot:Metabot):
 	metabot = mbot
-	
-func receive_input():
-	print("input")
 
+# https://docs.godotengine.org/en/stable/tutorials/inputs/inputevent.html
+# TODO handle platform specific input before this (ex. gamepad vs touch vs keyboard)
+# https://docs.godotengine.org/en/stable/classes/class_%40globalscope.html#enum-globalscope-keylist
+func send_input(input):
+	print("input: %s" % input)
+	inputs.appenmd(input)
+
+func process_input():
+	var input = inputs.pop_front()
+	
+	match input:
+		KEY_DOWN:
+			input = "down"	
+		KEY_UP:
+			input = "up"
+		KEY_RIGHT: 
+			input = "right"
+		KEY_LEFT:
+			input = "left"
+			
+	return input
+	
 func create_command():
 	print("command")
 	
 
+func send_signal():
+	print("signal")
+	
+func execute_action():
+	print("action")
+	
+func report_status():
+	print("agent status")
 
 # inner classes
 
