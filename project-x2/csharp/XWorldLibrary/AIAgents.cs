12d40b7888beed488d5f60a26ed27e4b9b1d7f96
Michael Garrido <michael.a.garrido@gmail.com>
2023-02-18 15:49:51 -0800

    adding A* library for AI Agents

--- project-x2/csharp/XWorldLibrary/AIAgents.cs
+++ project-x2/csharp/XWorldLibrary/AIAgents.cs
@@ -1,24 +1,50 @@
-using Active.Core;                
-using static Active.Raw;  
+using Active.Core;
+using static Active.Raw;
+
+using AStar.Options;
+using AStar;
 
 namespace XWorldLibrary;
 
-public class Soldier{
+public class Soldier
+{
 
-    public status Step(){
+    private WorldGrid _world;
+
+    void SetupPathfinding()
+    {
+        var level =   @"XXXXXXX
+                        X11X11X
+                        X11111X
+                        XXXXXXX";
+
+        _world = Helper.ConvertStringToPathfinderGrid(level);
+
+        var pathfinder = new PathFinder(_world);
+
+        var path = pathfinder.FindPath(new Position(1, 1), new Position(2, 3));
+
+        Helper.Print(_world, path);
+    }
+
+    public status Step()
+    {
         return Attack() || Defend() || Retreat();
     }
 
-    static status Attack(){
+    static status Attack()
+    {
         return done;
     }
 
-    static status Defend(){
+    static status Defend()
+    {
         return done;
-        
+
     }
 
-    static status Retreat(){
+    static status Retreat()
+    {
         return done;
     }
 
