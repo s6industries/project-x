29b70f7d935daa5e6d845c3fe18655b61015a971
Michael Garrido <michael.a.garrido@gmail.com>
2023-02-25 10:09:31 -0800

    prove Mac build uses XWorldLibrary outside of godot project

--- project-x2/test/TestControl.cs
+++ project-x2/test/TestControl.cs
@@ -3,12 +3,27 @@ using System;
 using XWorldLibrary;
 public partial class TestControl : Control
 {
+	public string info;
+	private Button _button;
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		GD.Print("_ready() TestControl.cs");
 		var mechaNode = new MechaNode();
 		GD.Print(mechaNode.Info());
+
+		info = mechaNode.Info();
+
+		_button = GetNode<Button>("Button");
+		_button.Text  = info;
+	}
+
+	public string GetInfo() {
+		var mechaNode = new MechaNode();
+		GD.Print(mechaNode.Info());
+		
+		return mechaNode.Info();
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
